2c516d9eef22af4f616f84b9d638bf03d0cd84c6
VoTaiTri <vo.tai.tri@framgia.com>
2015-01-19 10:01:07 +0700

    Finish user sugnup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup'  => 'users#new'
+ 
+  resources :users #get the routing for /users/id
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
